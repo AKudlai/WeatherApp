c4fae5c0b0c16350d6ee283362cd1c3e9344ffa1
Alexander Kudlai <student@kudlai.com>
2020-01-19 22:05:40 +0200

    Initial infrastructure

---
+++ Weather/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace Weather
+{
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/bootstrap")
+                .Include("~/Content/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/site")
+                .Include("~/Content/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.intellisense.js", 
+                    "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.bundle.min.js"));
+        }
+    }
+}
